ee356ec0f3b9a80212333b0fd84e1cb331fa3035
John Smith <iinexerjr@gmail.com>
2025-09-17 16:10:21 +0500

    ff

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Task4;
+Actor actor = new Actor("Tom Hardy", 45, "Male");
+
+Movie movie1 = new Movie("Inception", 2010);
+Movie movie2 = new Movie("Mad Max: Fury Road", 2015);
+
+actor.AddMovie(movie1);
+actor.AddMovie(movie2);
+Console.WriteLine("Возраст: " + actor.GetAge());
+Console.WriteLine("Пол: " + actor.GetGender());
+Console.WriteLine("Фильмы:");
